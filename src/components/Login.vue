fe22f7027d54869a211ebd00582d797c17231ccf
Lee <liyuehua@h3c.com>
2020-03-05 10:56:42 +0800

    完成了登陆功能

---
+++ src/components/Login.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt />
+      </div>
+      <!-- 登录表单区域 -->
+      <el-form ref= "loginFormRef" label-width="0px" :model="loginForm" class="login_form" :rules="loginFormRules">
+        <el-form-item prop="username">
+          <el-input prefix-icon="el-icon-user-solid" v-model="loginForm.username"></el-input>
+        </el-form-item>
+        <el-form-item prop= "password">
+          <el-input prefix-icon="el-icon-search" v-model="loginForm.password" type="password"></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="primary" @click= "login">登陆</el-button>
+          <el-button type="info" @click = "resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      loginForm: {
+        username: "",
+        password: ""
+      },
+      loginFormRules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
+        ]
+      }
+    };
+  },
+  methods: {
+      resetLoginForm(){
+          console.log(this.model)
+          this.$refs.loginFormRef.resetFields()
+      },
+      login(){
+          this.$refs.loginFormRef.validate(valid => {
+              if(!valid) return;
+              this.$message.error('登陆失败！')
+              window.sessionStorage.setItem("token","liyuehua")
+              this.$router.push('/home')
+          })
+      }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+.avatar_box {
+  height: 130px;
+  width: 130px;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  padding: 10px;
+  box-shadow: 0 0 10px #ddd;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background-color: #eee;
+  }
+}
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+</style>
